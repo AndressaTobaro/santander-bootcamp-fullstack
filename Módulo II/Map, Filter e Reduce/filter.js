651ecb37561419fdf5d20f0006719e2fdd59e81c
Andressa Tobaro <dessamatobaro@gmail.com>
2022-06-30 13:24:47 -0300

    Adicionando Map, Filter e Reduce (JS)

---
+++ Módulo II/Map, Filter e Reduce/filter.js
@@ -0,0 +1,15 @@
+function filtraPares(arr) {
+    return arr.filter(callback);
+}
+
+function callback(item) {
+    return item %2 === 0;
+}
+
+// function callback(item) {
+//     return item %2 !== 0;
+// } retorna impar
+
+const meuArray = [1, 23, 55, 67, 30, 2, 4];
+
+console.log(filtraPares(meuArray))
