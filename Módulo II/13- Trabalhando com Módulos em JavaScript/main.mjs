6d2d70888e4d9a7698e75e4ec16052741e957f68
Andressa Tobaro <dessamatobaro@gmail.com>
2022-07-27 20:40:55 -0300

    Adicionando Orientação a Objetos, e Trabalhando com Módulos em JS

---
+++ Módulo II/13- Trabalhando com Módulos em JavaScript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Andressa", 30));
+console.log(mostraCidade("Andressa", "Bauru"));
+console.log(mostraHobby("Andressa", "assistir séries"))
